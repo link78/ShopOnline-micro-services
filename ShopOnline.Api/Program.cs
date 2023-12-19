2d8ba165790b592ef2e7b66b41f57987b7983e65
Kader Derk <kaderderk@gmail.com>
2023-12-18 23:27:08 -0600

    product api

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories.Contracts;
 
 namespace ShopOnline.Api
 {
@@ -18,6 +19,9 @@ namespace ShopOnline.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddScoped<IProductRepository, IProductRepository>();
 
             var app = builder.Build();
 
---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext _context;
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this._context = shopOnlineDbContext;   
+        }
+        async Task<IEnumerable<ProductCategory>> IProductRepository.GetCategories()
+        {
+            var categories = await _context.ProductCategories.ToListAsync();
+
+            return categories;
+        }
+
+        Task<ProductCategory> IProductRepository.GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Product> IProductRepository.GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<IEnumerable<Product>> IProductRepository.GetItems()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
